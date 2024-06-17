943a89fe2957550f72d4cf3a79f307472ae001a5
chxnkm <ckangming0@gmail.com>
2024-06-17 19:04:54 +0800

    incremental updates to Navbar and Footer

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,15 +1,13 @@
-import Link from "next/link"
-import { Button } from "@/components/ui/button"
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 export default function Navbar() {
   return (
-    <nav className="flex inset-x-0 top-0 z-20 border-b-2 border-stone-400 bg-slate-200 shadow-sm dark:bg-gray-950/90">
-      <div className="w-full max-w-7xl mx-auto px-4">
+    <nav className="w-full inset-x-0 top-0 z-20 border-b-2 border-stone-400 bg-slate-200 shadow-sm dark:bg-gray-950/90">
+      <div className="max-w-7xl mx-auto px-4">
         <div className="flex justify-between h-20 items-center">
           <Link className="flex items-center" href="/">
-            
             <div className="flex items-center space-x-2">
               <Avatar>
                 <AvatarImage src="https://github.com/chxnkm.png" />
@@ -20,20 +18,20 @@ export default function Navbar() {
           </Link>
           <div className="flex items-center gap-4">
             <Button size="sm" variant="ghost">
-              <Link href ="/about-me">About Me</Link>
+              <Link href="/about-me">About Me</Link>
             </Button>
             <Button size="sm" variant="ghost">
-              <Link href ="/projects">Projects</Link>
+              <Link href="/projects">Projects</Link>
             </Button>
             <Button size="sm" variant="ghost">
-              <Link href ="#">Hobbies</Link>
+              <Link href="#">Hobbies</Link>
             </Button>
             <Button size="sm" variant="ghost">
-              <Link href ="/resume/RESUME_KANG_MING.pdf">Resume</Link>
+              <Link href="/resume/RESUME_KANG_MING.pdf">Resume</Link>
             </Button>
           </div>
         </div>
       </div>
     </nav>
-  )
+  );
 }
