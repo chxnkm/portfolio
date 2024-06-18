efb0d759a37bc85b879428c074108d9178c12e3c
chxnkm <ckangming0@gmail.com>
2024-06-18 16:24:06 +0800

    upholstered front page, make some css changes, cleanup

---
+++ app/hobbies/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import Link from "next/link"
+import DownloadCard from '@/components/ui/DownloadCard';
+
+
+export default function Home() {
+    return (
+      <main>
+       
+      </main>
+    );
+  }
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import AutoCarousel from '@/components/AutoCarousel';
+import Image from 'next/image';
 
 const images = [
   { src: 'img1.jpg', href: '/projects/internship' },
@@ -13,28 +14,25 @@ const images = [
 export default function Home() {
   return (
     <main>
+      <div className='w-screen bg-pastelBeige border-b-4 border-dcYellow'>
       <div className="intro container mx-auto">
-        <div className='flex'>
+        <div className='flex gap-12'>
           <div className="intro-words flex-1">
-            <h1 className="container mt-32 text-6xl">
-              Hello, I'm <span className="text-pastelBlue"><br/>Kang Ming.</span>
+            <h1 className="container mt-32 text-6xl font-belsey font-black">
+              Hello, I'm <span className="text-dcYellow leading-custom"><br/>Kang Ming.</span>
             </h1>
-            <p className="container text-2xl font-medium">I'm a <strong><span className="text-pastelGreen">Computer Science graduate</span>, a <span className="text-pastelRed">software engineer</span>, and an <span className="text-pastelOrange">avid tinkerer</span>.</strong></p>
+            <p className="container text-2xl font-medium">I'm a <strong><span className="text-pastelGreen">Computer Science graduate</span>, a<br/> <span className="text-pastelRed">software engineer</span>, and an <span className="text-pastelOrange">avid tinkerer</span>.</strong></p>
             <p className="container text-xl font-medium">Welcome to my portfolio. Happy browsing!</p>
           </div>
-        <div className='intro-image flex-1 items-center justify-center mt-12'>
-          <AutoCarousel basePath='/img/' images={images}/>
-          {/* <Image
-              src={'/img/splash.jpg'}
-              alt={'lolie'}
-              width={640}
-              height={480}
-            /> */}
-        </div>
+          <div className='intro-image flex-1 items-center justify-center my-12 pl-8'>
+        <div style={{ width: '480px', height: '480px', backgroundImage: 'url("/img/misc/main_splash.jpg")', backgroundSize: 'cover', backgroundPosition: 'center', borderRadius: '24px'}}></div>
+      </div>
       </div>
         
         
       </div>
+      </div>
+      
       <div className="container mx-auto">
         <div className="flex">
           <div className="body flex-1  p-8">
