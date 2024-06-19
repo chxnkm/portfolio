3bb566c9ddd4d6d6790820d46fb30eff553f9412
chxnkm <ckangming0@gmail.com>
2024-06-19 15:50:47 +0800

    added a simple server for the project cards - might move next time

---
+++ app/projects/fyp/page.tsx
@@ -0,0 +1,60 @@
+import Image from 'next/image'
+
+
+export default function Home() {
+    return (
+      <main>
+        <div className="text container mx-auto">
+            <div className="mt-32 grid grid-cols-2 gap-24">
+                    <div>
+                        <div className="docCentral flex flex-col items-center">
+                            <Image src="/img/img1.jpg" width={600} height={900} alt="docCentral intro"/>
+                            <p className='mt-2 font-medium'>Me and the docCentral team!</p>
+                        </div>
+                    </div>
+                    <div>
+                        <h1 className="col-span-2 lg:col-start-2 text-left">Final Year Project</h1>
+                        <p className="text-lg col-span-2 lg:col-start-2 text-justify">
+                            In the beginning of 2023, I was grateful to be offered the opportunity to intern at the CPF Board under the <strong>Frontier Products Team.</strong>
+                            <br /><br/>
+                            In my short 5-month tenure, I have been greatly exposed to industrial-standard full-stack development, and had the chance to be part of two projects - a<strong> QA-bot </strong> leveraging <strong>GPT technologies,</strong> and docCentral, a <strong>full-stack document templating webapp and plugin</strong> for Microsoft Word.
+                        </p>    
+                    </div>
+            </div>
+            <div className="mt-32 grid grid-cols-2 gap-24">
+                    <div>
+                        <div className="docCentral flex flex-col items-center">
+                            <Image src="/img/internship/chatcentral.jpg" width={600} height={900} alt="docCentral intro"/>
+                            <p className='mt-2 font-medium'>GPT-powered QA Bot in practice.</p>
+                        </div>
+                    </div>
+                    <div>
+                    <h1 className="text-4xl col-span-2 lg:col-start-2 text-left"><span className="text-qaPurple">GPT-powered QA Bot 🤖</span></h1>
+                        <p className="text-lg col-span-2 lg:col-start-2 text-justify">
+                            One of the products I worked on was spear-heading my team's large-language model (LLM) stack. We decided to venture and try out to create a GPT-powered question-answering bot capable of answering user-inputted queries about CPF. I worked on many aspects of the project, first by understanding the system of a language model, and started by collecting data from the CPF website via website automated website scraping with <strong>Selenium</strong>.
+                            <br/><br/>
+                            Leveraging the <strong>OpenAI GPT API</strong>, as well as a simple React-stack application, we were able to create a proof-of-concept, which received very positive feedback from the board of directors. We even presented the product in a inter-department sharing, where it also similarly was received very positively.
+                        </p>   
+                    </div>
+            </div>
+            <div className="mt-32 grid grid-cols-2 gap-24">
+                    <div>
+                        <div className="docCentral flex flex-col items-center">
+                            <Image src="/img/internship/guide.gif" width={600} height={900} alt="docCentral intro" unoptimized={true}/>
+                            <p className='mt-2 font-medium'>Screengrabs of the guide for docCentral.</p>
+                        </div>
+                    </div>
+                    <div>
+                    <h1 className="text-4xl col-span-2 lg:col-start-2 text-left"><span className="text-dcYellow">docCentral 📑</span></h1>
+                        <p className="text-lg col-span-2 lg:col-start-2 text-justify">
+                            docCental is a document templating plugin for Microsoft Word that enables government officers to create documents from templates in a seamless and time-saving method. It consists of a Next.js-based webapp and a Microsoft Word XML Add-in. It taps on FormSG as an input for the document, which can be templated through the add-in, and outputs a completed document to one's desired email.
+                            <br/><br/>
+                            The main motivations of creating docCentral was to reduce repetitive document filling, where most of the document does not change, and only pertinent fields like the name, date and so on need to be modified. It streamlines workflow and improves productivity. Upon rolling out in a pilot to various interested governmental agencies, many express great interest in using the product.
+
+                        </p>   
+                    </div>
+            </div>
+        </div>
+      </main>
+    );
+  }
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,37 +1,60 @@
-import Image from 'next/image'
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
+import fs from 'fs';
+import path from 'path';
+import { Card, CardHeader, CardTitle, CardDescription, CardContent} from '@/components/ui/card';
+
+type Project = {
+  date: Date;
+  name: string;
+  description: string;
+  image: string;
+  href: string;
+};
+
+const fetchProjects = async (): Promise<Project[]> => {
+  const filePath = path.join(process.cwd(), 'data', 'projects.json');
+  const jsonData = fs.readFileSync(filePath, 'utf-8');
+  const projectsData = JSON.parse(jsonData).projects;
+
+  // Parse date strings into Date objects
+  const projects = projectsData.map((project: any) => ({
+    ...project,
+    date: new Date(project.date),
+  })) as Project[];
+
+  // Sort projects in descending order based on date
+  projects.sort((a, b) => b.date.getTime() - a.date.getTime());
+
+  return projects;
+};
 
 
-export default function Home() {
-    return (
-      <main>
-        <div className="text container mx-auto">
-          <div className="col-span-2 lg:col-start-2 text-right mt-32 gap-8">
-            <h1>My Projects 👨🏻‍💻</h1>
-            <p className="text-xl mt-12">Click on each card to learn more!</p>
-          </div>
-          <div className='mt-12'>
-            <Card>
-              <CardHeader>
-                <CardTitle>Internship at CPF Board</CardTitle>
-                <CardDescription>Card Description</CardDescription>
-              </CardHeader>
-              <CardContent>
-                <p>Card Content</p>
-              </CardContent>
-              <CardFooter>
-                <p>Card Footer</p>
-              </CardFooter>
-            </Card>
-          </div>
+const Projects = async () => {
+  const projects = await fetchProjects();
+
+  return (
+    <main>
+      <div className="text container mx-auto">
+        <div className="col-span-2 lg:col-start-2 text-right mt-32 gap-8">
+          <h1 className="font-black font-belsey">My Projects 👨🏻‍💻</h1>
+          <p className="text-xl mt-12">Click on each card to learn more!</p>
         </div>
-      </main>
-    );
-  }
+        <div className='mt-12'>
+          {projects.map((project, index) => (
+            <Card key={index} className="border-4 mt-4">
+              <CardHeader>
+                <CardTitle><a href={project.href} className="font-belsey text-3xl hover:underline">{project.name}</a></CardTitle>
+                <CardDescription>{project.date.toDateString().split(' ').slice(1).join(' ')}</CardDescription>
+              </CardHeader>
+              <CardContent className="flex items-center justify-between mt-[-5%]">
+                <p className="flex-grow max-w-[75%]">{project.description}</p>
+                <div style={{ width: '150px', height: '150px', backgroundImage: `url(${project.image})`, backgroundSize: 'contain', backgroundRepeat: 'no-repeat', backgroundPosition: 'center'}}/>
+              </CardContent>
+            </Card>
+          ))}
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Projects;
