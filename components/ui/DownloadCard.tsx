5b971f5012d38a4f3d0e4aa6d6f499f6e7a4c974
chxnkm <ckangming0@gmail.com>
2024-06-23 23:29:05 +0800

    Updated resume

--- components/ui/DownloadCard.tsx
+++ components/ui/DownloadCard.tsx
@@ -8,10 +8,10 @@ interface DownloadCardProps {
 
 const DownloadCard: React.FC<DownloadCardProps> = ({ fileName, fileUrl }) => {
   return (
-    <Card className="w-full p-4 mt-8">
+    <Card className="w-full p-4 mt-8 bg-background border-1 border-[#bc7b0b]">
       <CardHeader className="flex justify-between items-center">
         <h3 className="text-lg font-semibold">{fileName}</h3>
-        <a href={fileUrl} download className="inline-block bg-blue-500 text-white hover:bg-blue-700 py-2 px-4 rounded">
+        <a href={fileUrl} download className="inline-block bg-[#bc7b0b] text-white py-2 px-4 rounded">
           Download
         </a>
       </CardHeader>
