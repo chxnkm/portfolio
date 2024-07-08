14af24721a2045361257462c2669959eb7411bf3
chxnkm <ckangming0@gmail.com>
2024-07-08 15:14:50 +0800

    Added social icons

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,9 @@ import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import EmailIcon from '@mui/icons-material/Email';
 
 const images = [
-  { src: 'misc/main_splash.jpg'},
-  { src: 'misc/splash_2.jpg'},
-  { src: 'misc/splash_3.jpg'},
+  { src: 'misc/main_splash.jpg' },
+  { src: 'misc/splash_2.jpg' },
+  { src: 'misc/splash_3.jpg' },
   // Add more images as needed
 ];
 
@@ -37,38 +37,38 @@ export default function Home() {
           <div className="mt-20 order-1 lg:order-1 flex justify-center lg:justify-start">
             <div className="flex flex-col items-center lg:items-start">
               <UnclickableImage width={480} src="/img/misc/me.jpg" alt="my face" />
-                  <div className="flex mt-4 space-x-4">
-                    <a href="https://github.com/your-username" target="_blank" rel="noopener noreferrer">
-                      <GitHubIcon style={{ fontSize: 40 }} />
-                    </a>
-                    <a href="https://linkedin.com/in/your-username" target="_blank" rel="noopener noreferrer">
-                      <LinkedInIcon style={{ fontSize: 40 }} />
-                    </a>
-                    <a href="mailto:your-email@example.com">
-                      <EmailIcon style={{ fontSize: 40 }} />
-                    </a>
-                  </div>
-                </div>
+              <div className="flex mt-4 space-x-4">
+                <a className='hover:scale-125 duration-300' href="https://github.com/chxnkm" target="_blank" rel="noopener noreferrer">
+                  <GitHubIcon style={{ fontSize: 24 }} />
+                </a>
+                <a className='hover:scale-125 duration-300' href="https://linkedin.com/in/ckangming" target="_blank" rel="noopener noreferrer">
+                  <LinkedInIcon style={{ fontSize: 24 }} />
+                </a>
+                <a className='hover:scale-125 duration-300' href="mailto:ckangming0.com">
+                  <EmailIcon style={{ fontSize: 24 }} />
+                </a>
+              </div>
             </div>
+          </div>
           <div className='order-2 lg:order-2'>
             <h1 className="text-center lg:text-left font-belsey text-2xl sm:text-3xl md:text-4xl lg:text-5xl">About Me 😄</h1>
             <p className="text-md lg:text-lg text-justify">
               Hello all, Kang Ming here. A graduate Computer Science student from Nanyang Technological University, I am an aspiring <strong>software engineer</strong> and <strong>data analyst</strong>.
-              <br /><br/>
+              <br /><br />
               With a multitude of projects I have worked on in my university tenure, I have amassed robust skills in <strong>full-stack development, data analytics,</strong> as well as <strong>machine learning and GPT-related technologies</strong>.
-              <br /><br/>
+              <br /><br />
               In my free time, I pursue photography (in both film and digital formats), jam to nostalgic tunes with my guitar and drums, and am an avid <i>Fallout</i> franchise lover.
-              <br/><br/>
+              <br /><br />
               Currently, I am open to any software-related full-time opportunities in Singapore. Please feel free to <a href='/contact' className="underline text-blue-500">contact me</a> using this form.
             </p>
           </div>
         </div>
       </div>
       <div id='projects' className=" mt-[6vh] container mx-auto animate-slideUp">
-          <h1 className="text-4xl font-black font-belsey">Projects and Work Experience 👨🏻‍💻</h1>
-          <p className="text-lg mt-6">Click on each card to learn more!</p>
+        <h1 className="text-4xl font-black font-belsey">Projects and Work Experience 👨🏻‍💻</h1>
+        <p className="text-lg mt-6">Click on each card to learn more!</p>
       </div>
-      <Projects/>
+      <Projects />
     </main>
   );
 }
