1c34ded2f3eb9a73619f1f1e3f2ad9d21008eb73
chxnkm <ckangming0@gmail.com>
2024-07-09 18:05:54 +0800

    Added skeleton for loading projects

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -19,6 +19,8 @@ import {
   PaginationPrevious,
 } from "@/components/ui/pagination"
 
+import { Skeleton } from "@/components/ui/skeleton"
+
 import { Project } from '@/lib/projects-api';
 
 const ITEMS_PER_PAGE = 3;
@@ -27,13 +29,16 @@ const Projects = () => {
   const [projects, setProjects] = useState<Project[]>([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [numPages, setNumPages] = useState(1);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchProjects = async () => {
+      setIsLoading(true);
       const response = await fetch('/api/fetch-projects');
       const data = await response.json();
       setProjects(data);
       setNumPages(Math.ceil(data.length / ITEMS_PER_PAGE));
+      setIsLoading(false);
     };
 
     fetchProjects();
@@ -43,48 +48,82 @@ const Projects = () => {
   const indexOfFirstProject = indexOfLastProject - ITEMS_PER_PAGE;
   const currentProjects = projects.slice(indexOfFirstProject, indexOfLastProject);
 
+  const SkeletonProject = () => (
+    <Card className="bg-[#fdfdfd] border-gray-200 mt-4 shadow-lg rounded-lg overflow-hidden">
+      <CardHeader>
+        <Skeleton className="h-6 w-[50%]" />
+        <Skeleton className="h-4 w-12 mt-4" />
+      </CardHeader>
+      <CardContent className="flex flex-wrap items-start justify-between">
+        <div className="flex flex-col flex-grow max-w-[75%] mt-[2vh]">
+          <Skeleton className="h-4 w-full mb-2" />
+          <Skeleton className="h-4 w-full mb-2" />
+          <Skeleton className="h-4 w-3/4" />
+          <div className="flex flex-wrap items-center justify-start gap-2 mt-8">
+            <Skeleton className="h-6 w-16" />
+            <Skeleton className="h-6 w-16" />
+            <Skeleton className="h-6 w-16" />
+          </div>
+        </div>
+        <div className='mt-4'>
+          <Skeleton className="h-[180px] w-[180px]" />
+        </div>
+      </CardContent>
+    </Card>
+  );
+
   return (
     <main>
       <div className="text container mx-auto">
         <div className='mt-8'>
-          {currentProjects.map((project, index) => (
-            <a href={project.href} key={index}>
-              <Card className="bg-[#fdfdfd] border-gray-200 mt-4 shadow-lg rounded-lg overflow-hidden animate-slideUp">
-                <CardHeader>
-                  <CardTitle className="font-belsey text-3xl">{project.name}</CardTitle>
-                  <CardDescription>{new Date(project.date).toDateString().split(' ').slice(1).join(' ')}</CardDescription>
-                </CardHeader>
-                <CardContent className="flex flex-wrap items-start justify-between mt-[-3%]">
-                  <div className="flex flex-col flex-grow max-w-[75%] mt-[2vh]">
-                    <p>{project.description}</p>
-                    <div className="flex flex-wrap font-belsey font-medium items-center justify-start gap-2 mt-8">
-                      <strong>Skills:</strong>
-                      {project.skills.map((skill, skillIndex) => (
-                        <div key={skillIndex} className="flex items-center justify-center bg-pastelBeige py-1 md:px-3 px-2 rounded-md whitespace-nowrap text-xs md:text-sm">
-                          {skill}
-                        </div>
-                      ))}
-                    </div>
-                  </div>
-                  <div className='mt-4'>
-                  <div style={{ width: '180px', height: '180px', backgroundImage: `url(${project.image})`, backgroundSize: 'contain', backgroundRepeat: 'no-repeat', backgroundPosition: 'center' }} />
-                  </div>
-                </CardContent>
-              </Card>
-            </a>
-          ))}
-        </div>
-        <Pagination className="mt-8" aria-label="Pagination">
-          <PaginationPrevious onClick={() => setCurrentPage(prev => Math.max(prev - 1, 1))}>Previous</PaginationPrevious>
-          <PaginationContent>
-            {Array.from({ length: numPages }, (_, i) => (
-              <PaginationItem key={i}>
-                <PaginationLink onClick={() => setCurrentPage(i + 1)}>{i + 1}</PaginationLink>
-              </PaginationItem>
+          {isLoading ? (
+            <>
+            {Array.from({ length: ITEMS_PER_PAGE }).map((_, index) => (
+              <SkeletonProject key={index} />
             ))}
-          </PaginationContent>
-          <PaginationNext onClick={() => setCurrentPage(prev => Math.min(prev + 1, numPages))}>Next</PaginationNext>
-        </Pagination>
+            </>
+          ) : (
+            currentProjects.map((project, index) => (
+              <a href={project.href} key={index}>
+                <Card className="bg-[#fdfdfd] border-gray-200 mt-4 shadow-lg rounded-lg overflow-hidden animate-slideUp">
+                  <CardHeader>
+                    <CardTitle className="font-belsey text-3xl">{project.name}</CardTitle>
+                    <CardDescription>{new Date(project.date).toDateString().split(' ').slice(1).join(' ')}</CardDescription>
+                  </CardHeader>
+                  <CardContent className="flex flex-wrap items-start justify-between mt-[-3%]">
+                    <div className="flex flex-col flex-grow max-w-[75%] mt-[2vh]">
+                      <p>{project.description}</p>
+                      <div className="flex flex-wrap font-belsey font-medium items-center justify-start gap-2 mt-8">
+                        <strong>Skills:</strong>
+                        {project.skills.map((skill, skillIndex) => (
+                          <div key={skillIndex} className="flex items-center justify-center bg-pastelBeige py-1 md:px-3 px-2 rounded-md whitespace-nowrap text-xs md:text-sm">
+                            {skill}
+                          </div>
+                        ))}
+                      </div>
+                    </div>
+                    <div className='mt-4'>
+                    <div style={{ width: '180px', height: '180px', backgroundImage: `url(${project.image})`, backgroundSize: 'contain', backgroundRepeat: 'no-repeat', backgroundPosition: 'center' }} />
+                    </div>
+                  </CardContent>
+                </Card>
+              </a>
+            ))
+          )}
+        </div>
+        {!isLoading && (
+          <Pagination className="mt-8" aria-label="Pagination">
+            <PaginationPrevious onClick={() => setCurrentPage(prev => Math.max(prev - 1, 1))}>Previous</PaginationPrevious>
+            <PaginationContent>
+              {Array.from({ length: numPages }, (_, i) => (
+                <PaginationItem key={i}>
+                  <PaginationLink onClick={() => setCurrentPage(i + 1)}>{i + 1}</PaginationLink>
+                </PaginationItem>
+              ))}
+            </PaginationContent>
+            <PaginationNext onClick={() => setCurrentPage(prev => Math.min(prev + 1, numPages))}>Next</PaginationNext>
+          </Pagination>
+        )}
       </div>
     </main>
   );
