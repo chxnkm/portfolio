8c9aa08d6a95a37dd12181a857c88f0ee27d1a6c
chxnkm <ckangming0@gmail.com>
2024-07-15 03:21:31 +0800

    init firebase stoage

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    images: {
+      remotePatterns :[
+        {
+          protocol: 'https',
+          hostname: 'firebasestorage.googleapis.com',
+          pathname: '**'
+        }
+      ]
+    }
   }
 export default nextConfig;
