b49262453ec638a499547d607f46ed2d3f23297c
chxnkm <ckangming0@gmail.com>
2025-05-02 23:48:37 +0800

    fix: minor alignment to arrow

--- app/projects/layout.tsx
+++ app/projects/layout.tsx
@@ -8,7 +8,7 @@ export default function DashboardLayout({
       <div className="sm:max-w-[75vw] mx-auto px-4 sm:px-0">
         <a href="/#projects" rel="noopener noreferrer">
           <button className="mt-8 px-4 py-2 text-sm font-medium text-gray-700 bg-gray-100    hover:bg-gray-200 dark:hover:bg-gray-700 rounded-lg transition-colors duration-200 flex items-center gap-2">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-left" viewBox="0 0 16 16">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-left mt-[0.1rem]" viewBox="0 0 16 16">
               <path fillRule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
             </svg>
             Back to Projects
